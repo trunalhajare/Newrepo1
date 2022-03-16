b37f5388ce467cbd1fa83060f64b990906b6e20e
trunalhajare <101618311+trunalhajare@users.noreply.github.com>
2022-03-16 10:28:16 +0530

    Add files via upload

---
+++ Sim.java
@@ -0,0 +1,11 @@
+class NoteTest {
+public static void main (String[] args){
+
+System.out.println("Hello world");
+
+System.out.println("hello01");
+
+System.out.println("etc");
+
+System.out.println("batch B");
+}}
